b4c026eae6175499bf7c8f27fc6ad52cb1232c77
Mellijo <118224647+Mellijo@users.noreply.github.com>
2023-01-12 21:14:43 +0000

    started the forecast bar

--- src/app.js
+++ src/app.js
@@ -56,6 +56,31 @@ function handleSubmit(event) {
   search(searchCityElement.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thur", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img src="" alt="">
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">14°C</span>
+              <span class="weather-forecast-temperature-min">10°C</span>
+            </div>
+            </div>
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+displayForecast();
 search("New York");
 
 let form = document.querySelector("#search-form");
