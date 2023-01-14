5e57c763c8cc0855d01bfe47ebf7d05324ea52b4
Mellijo <118224647+Mellijo@users.noreply.github.com>
2023-01-14 21:41:30 +0000

    adding api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 function formatDate(timestamp) {
-  console.log("formatDate", timestamp);
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
@@ -23,7 +22,57 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  //let days = ["Thur", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay) {
+    console.log({ forecastDay });
+    forecastHTML =
+      forecastHTML +
+      ` <div class="col-2">
+          <div class="weather-forecast-date">${formatDate(
+            forecastDay.time * 1000
+          )}</div>
+        
+       <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" alt="" width="42"
+  
+
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max">${Math.round(
+                forecastDay.temperature.maximum
+              )}°C</span>
+              <span class="weather-forecast-temperature-min">${Math.round(
+                forecastDay.temperature.minimum
+              )}°C</span>
+            </div>
+            </div>
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "6a4024581cfab2ad0f49c537bof0td13";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon={coordinates.lon}&lat={coordinates.lat}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
+  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -31,17 +80,17 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-  console.log(response);
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-
   dateElement.innerHTML = formatDate(response.data.time * 1000);
   iconElement.setAttribute("src", response.data.condition.icon_url);
   //iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coordinates);
 }
 
 function search(city) {
@@ -56,31 +105,6 @@ function handleSubmit(event) {
   search(searchCityElement.value);
 }
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Thur", "Fri", "Sat", "Sun", "Mon"];
-
-  let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-          <img src="" alt="">
-            <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">14°C</span>
-              <span class="weather-forecast-temperature-min">10°C</span>
-            </div>
-            </div>
-            `;
-  });
-
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-
-displayForecast();
 search("New York");
 
 let form = document.querySelector("#search-form");
